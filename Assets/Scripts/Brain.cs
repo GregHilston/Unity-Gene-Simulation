d44a1a3c814103be3395b553f33c7ac39876a2b8
GregHilston <Gregory.Hilston@gmail.com>
2019-07-05 14:37:17 -0600

    brain activity being decided

--- Assets/Scripts/Brain.cs
+++ Assets/Scripts/Brain.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brain : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(Hunger))]
+[RequireComponent(typeof(Thirst))]
+[RequireComponent(typeof(Reproduce))]
+public class Brain : MonoBehaviour {
+    enum Activity {
+        WANDERING,
+        FOOD_SEARCH,
+        WATER_SEARCH,
+        MATE_SEARCH,
+        EATING,
+        DRINKING,
+        BIRTHING
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField]
+    private Activity currentActivity = Activity.WANDERING;
+    private Hunger hunger;
+    private Thirst thirst;
+    private Reproduce reproduce;
+
+    void Start() {
+        this.hunger = GetComponent<Hunger>();
+        this.thirst = GetComponent<Thirst>();
+        this.reproduce = GetComponent<Reproduce>();
+    }
+
+    private Activity decideActivity() {
+        if(this.hunger != null && this.thirst != null && this.reproduce != null) {
+            if(this.hunger.currentHunger >= this.thirst.currentThirst && this.hunger.currentHunger >= this.reproduce.currentReproduce) {
+                return Activity.FOOD_SEARCH;
+            }
+
+            if (this.thirst.currentThirst >= this.hunger.currentHunger && this.thirst.currentThirst >= this.reproduce.currentReproduce) {
+                return Activity.WATER_SEARCH;
+            }
+
+            if (this.reproduce.currentReproduce >= this.hunger.currentHunger && this.reproduce.currentReproduce >= this.thirst.currentThirst) {
+                return Activity.MATE_SEARCH;
+            }
+        }
+
+        // Default cause
+        return Activity.WANDERING;
+    }
+
+    void Update() {
+        this.currentActivity = this.decideActivity();
     }
 }
