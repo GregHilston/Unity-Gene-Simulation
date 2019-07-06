ea10a5c06318b1257481880d1810e65bf9c43c33
GregHilston <Gregory.Hilston@gmail.com>
2019-07-06 14:33:23 -0600

    hunting for water successfully. the code is nasty

--- Assets/Scripts/PluggableAI/Actions/HuntWaterAction.cs
+++ Assets/Scripts/PluggableAI/Actions/HuntWaterAction.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 [CreateAssetMenu(menuName = "PluggableAI/Actions/HuntWaterAction")]
 public class HuntWaterAction : Action {
     private Vector3? destination;
+    private GameObject waterTooDrink;
     private StateController lastStateController;
 
     private Vector3 randomNavCircle(StateController stateController, Vector3 origin, float distance, int layermask) {
@@ -19,13 +20,20 @@ public class HuntWaterAction : Action {
         if (stateController.getGenes() != null) {
             this.lastStateController = stateController;
 
-            if (this.destination == null) {
+            if (this.waterTooDrink == null) {
                 Collider[] hitColliders = Physics.OverlapSphere(stateController.transform.position, stateController.getGenes().radiusOfSight);
                 for(int i = 0; i < hitColliders.Length; i++) {
                     if(hitColliders[i].gameObject.GetComponent<Water>() != null) {
                         this.destination = hitColliders[i].gameObject.transform.position;
+                        this.waterTooDrink = hitColliders[i].gameObject;
                         break;
                     }
+                }
+
+                // We failed to find water, so we'll move somewhere random only if we've arrived at that random spot already
+                if(this.destination == null) {
+                    int allLayers = -1;
+                    this.destination = this.randomNavCircle(stateController, stateController.transform.position, stateController.getGenes().radiusOfSight, allLayers);
                 }
             }
 
@@ -34,6 +42,8 @@ public class HuntWaterAction : Action {
 
             if (stateController.transform.position == this.destination) {
                 this.destination = null;
+                Destroy(this.waterTooDrink);
+                this.waterTooDrink = null;
             }
         }
     }
