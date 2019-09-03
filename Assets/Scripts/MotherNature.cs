ed92f68bceef1df81b09b0463a3e46b4cca12006
GregHilston <Gregory.Hilston@gmail.com>
2019-09-02 19:59:24 -0600

    putting food and water under parents

--- Assets/Scripts/MotherNature.cs
+++ Assets/Scripts/MotherNature.cs
@@ -12,7 +12,11 @@ public class MotherNature : MonoBehaviour {
     [SerializeField]
     GameObject waterPrefab;
     [SerializeField]
+    GameObject waterParent;
+    [SerializeField]
     GameObject foodPrefab;
+    [SerializeField]
+    GameObject foodParent;
 
     void Start() {
         InvokeRepeating("placeWater", 0.0f, this.secondsToPlaceWater);
@@ -21,21 +25,22 @@ public class MotherNature : MonoBehaviour {
 
     private Vector3 randomPointInSquare(Vector3 center, Vector3 size) {
        float groundFloor = 0.0f;
-       return center + new Vector3((Random.value - 0.5f) * size.x, groundFloor, (Random.value - 0.5f) * size.z);
+       return center + new Vector3(Random.value * size.x, groundFloor, Random.value * size.z);
     }
 
-    private void placeGameObject(GameObject gameObject, Vector3 location) {
-        if(gameObject != null) {
-            Instantiate(gameObject, location, Quaternion.identity);
-            Debug.Log("placed " + gameObject.name + " at " + location);
+    private void placeGameObject(GameObject parent, GameObject prefab, Vector3 location) {
+        if(prefab != null) {
+            GameObject instantiatedGameObject = Instantiate(prefab, location, Quaternion.identity);
+            instantiatedGameObject.transform.parent = parent.transform;
+            Debug.Log("placed " + instantiatedGameObject.name + " at " + location);
         }
     }
 
     private void placeWater() {
-        this.placeGameObject(this.waterPrefab, this.randomPointInSquare(this.field.transform.position, this.field.transform.transform.localScale));
+        this.placeGameObject(this.waterParent, this.waterPrefab, this.randomPointInSquare(this.field.transform.position, this.field.transform.transform.localScale));
     }
 
     private void placeFood() {
-        this.placeGameObject(this.foodPrefab, this.randomPointInSquare(this.field.transform.position, this.field.transform.transform.localScale));
+        this.placeGameObject(this.foodParent, this.foodPrefab, this.randomPointInSquare(this.field.transform.position, this.field.transform.transform.localScale));
     }
 }
